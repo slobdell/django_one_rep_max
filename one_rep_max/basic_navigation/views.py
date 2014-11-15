5a40d43916b4b57703c7c8a27be42c63706f1d47
Scott Lobdell <slobdell@hearsaycorp.com>
2014-11-15 13:02:57 -0800

    add static pages

--- one_rep_max/basic_navigation/views.py
+++ one_rep_max/basic_navigation/views.py
@@ -7,11 +7,39 @@ from django.shortcuts import render_to_response
 from one_rep_max.stripe.constants import get_publishable_key
 
 
+GOOGLE_UGLY_URL = '_escaped_fragment_'
+
+
 def render_to_json(data, status=200):
     return HttpResponse(json.dumps(data), content_type="application/json", status=status)
 
 
+def static_pages_for_crawlers(request):
+    requested_page = request.GET[GOOGLE_UGLY_URL]
+    requested_page_to_static_file = {
+        'account': 'account.html',
+        'about': 'about.html',
+        'contact': 'contact.html'
+    }
+    '''
+        "!orientation": "orientationView",
+        "!youtube/:videoId": "youtube",
+        "!account": "accountView",
+        "!thankyou": "thankYouView",
+        "!summary": "orderSummaryView",
+        "!upload": "uploadView",
+    '''
+    static_file = requested_page_to_static_file[requested_page]
+    return render_to_response("static_pages/%s" % static_file, {})
+
+
 def home(request):
+    if GOOGLE_UGLY_URL in request.GET:
+        try:
+            return static_pages_for_crawlers(request)
+        except KeyError:
+            pass  # non-existent page
+
     youtube_video_ids = (
         ('7EUZblF_ObA', 724),
         ('eLYxnhlPFtc', 297),
